a616d4cd929f309e13e86c4c3ffe5508381e7c47
Yuksel Kurtbas <yukselk@kainos.com>
2018-05-04 15:59:06 +0100

    [LPFG-49][LPFG-278][BUG] Missing resource

---
+++ src/main/java/uk/gov/cshr/civilservant/resource/CivilServantResource.java
@@ -0,0 +1,47 @@
+package uk.gov.cshr.civilservant.resource;
+
+import uk.gov.cshr.civilservant.domain.*;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class CivilServantResource {
+
+    private String fullName;
+
+    private Grade grade;
+
+    private Organisation organisation;
+
+    private Profession profession;
+
+    private JobRole jobRole;
+
+    public CivilServantResource(CivilServant civilServant) {
+        checkArgument(civilServant != null);
+        this.fullName = civilServant.getFullName();
+        this.grade = civilServant.getGrade();
+        this.organisation = civilServant.getOrganisation();
+        this.profession = civilServant.getProfession();
+        this.jobRole = civilServant.getJobRole();
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public Organisation getOrganisation() {
+        return organisation;
+    }
+
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public JobRole getJobRole() {
+        return jobRole;
+    }
+}
