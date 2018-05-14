dda3d568e6f6cf62dc11920cc8ca7f34c92915eb
Yuksel Kurtbas <yukselk@kainos.com>
2018-05-14 15:37:20 +0100

    [LPFG-318][LPFG-323][CHORE] Update packages with fixed versions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,5 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
-TRUNCATE TABLE `civil_servant`;
-TRUNCATE TABLE `department`;
-TRUNCATE TABLE `organisation`;
-TRUNCATE TABLE `grade`;
-TRUNCATE TABLE `identity`;
-TRUNCATE TABLE `profession`;
-TRUNCATE TABLE `job_role`;
-
 INSERT INTO grade (code, name, organisation_id) VALUES
   ('G6', 'Grade 6', null),
   ('G7', 'Grade 7', null);
