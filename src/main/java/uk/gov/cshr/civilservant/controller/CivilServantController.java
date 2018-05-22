bf9d261494961d88d0e775ca5eeae05c68f436e9
matthewmu <matthewmu@kainos.com>
2018-05-22 09:43:21 +0100

    Get civil servant by uid, dev inserts for identity and cs

--- src/main/java/uk/gov/cshr/civilservant/controller/CivilServantController.java
+++ src/main/java/uk/gov/cshr/civilservant/controller/CivilServantController.java
@@ -1,5 +1,6 @@
 package uk.gov.cshr.civilservant.controller;
 
+import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,13 @@ import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.cshr.civilservant.domain.CivilServant;
+import uk.gov.cshr.civilservant.domain.Identity;
 import uk.gov.cshr.civilservant.repository.CivilServantRepository;
+import uk.gov.cshr.civilservant.repository.IdentityRepository;
+import uk.gov.cshr.civilservant.repository.InternalCivilServantRepository;
 import uk.gov.cshr.civilservant.resource.CivilServantResource;
 
 import java.util.ArrayList;
@@ -25,6 +30,7 @@ import java.util.Optional;
 import static com.google.common.base.Preconditions.checkArgument;
 
 @RepositoryRestController
+@ExposesResourceFor(CivilServant.class)
 @RequestMapping("/civilServants")
 public class CivilServantController implements ResourceProcessor<RepositoryLinksResource> {
 
@@ -34,13 +40,23 @@ public class CivilServantController implements ResourceProcessor<RepositoryLinks
 
     private RepositoryEntityLinks repositoryEntityLinks;
 
+    private InternalCivilServantRepository internalCivilServantRepository;
+
+    private IdentityRepository identityRepository;
+
     @Autowired
     public CivilServantController(CivilServantRepository civilServantRepository,
-                                  RepositoryEntityLinks repositoryEntityLinks) {
+                                  RepositoryEntityLinks repositoryEntityLinks,
+                                  InternalCivilServantRepository internalCivilServantRepository,
+                                  IdentityRepository identityRepository) {
         checkArgument(civilServantRepository != null);
         checkArgument(repositoryEntityLinks != null);
+        checkArgument(internalCivilServantRepository != null);
+        checkArgument(identityRepository != null);
         this.civilServantRepository = civilServantRepository;
         this.repositoryEntityLinks = repositoryEntityLinks;
+        this.internalCivilServantRepository = internalCivilServantRepository;
+        this.identityRepository = identityRepository;
     }
 
     @GetMapping
@@ -59,6 +75,31 @@ public class CivilServantController implements ResourceProcessor<RepositoryLinks
 
         Optional<CivilServant> optionalCivilServant = civilServantRepository.findByPrincipal();
 
+        return getResourceResponseEntity(optionalCivilServant);
+    }
+
+    @GetMapping("/{uid}")
+    public ResponseEntity<Resource<CivilServantResource>> getById(@PathVariable("uid") String uid) {
+        LOGGER.debug("Getting civil servant details for user with uid {}", uid);
+
+        Optional<Identity> identity = identityRepository.findByUid(uid);
+
+        if(!identity.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Optional<CivilServant> optionalCivilServant = internalCivilServantRepository.findByIdentity(identity.get());
+
+        return getResourceResponseEntity(optionalCivilServant);
+    }
+
+    @Override
+    public RepositoryLinksResource process(RepositoryLinksResource resource) {
+        resource.add(ControllerLinkBuilder.linkTo(CivilServantController.class).withRel("civilServants"));
+        return resource;
+    }
+
+    private ResponseEntity<Resource<CivilServantResource>> getResourceResponseEntity(Optional<CivilServant> optionalCivilServant) {
         return optionalCivilServant
                 .map(civilServant -> {
                     Resource<CivilServantResource> resource = new Resource<>(new CivilServantResource(civilServant));
@@ -70,11 +111,5 @@ public class CivilServantController implements ResourceProcessor<RepositoryLinks
                     return ResponseEntity.ok(resource);
                 })
                 .orElse(ResponseEntity.notFound().build());
-    }
-
-    @Override
-    public RepositoryLinksResource process(RepositoryLinksResource resource) {
-        resource.add(ControllerLinkBuilder.linkTo(CivilServantController.class).withRel("civilServants"));
-        return resource;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO department (code, name) VALUES
   ('co', 'Cabinet Office');
 
 INSERT INTO organisation (code, name, department_id) VALUES
-  ('co', 'Cabinet Office', (SELECT id FROM department WHERE code = 'co'));
+  ('co', 'Cabinet Office', SELECT id FROM department WHERE code = 'co');
 
 INSERT INTO profession (name) VALUES
   ('Commercial'),
@@ -24,33 +24,50 @@ INSERT INTO profession (name) VALUES
   ('Property'),
   ('Other');
 
-SELECT id INTO @profession FROM profession WHERE name = 'Commercial';
-
 INSERT INTO job_role (name, profession_id) VALUES
-  ('Strategy and Policy Development', @profession),
-  ('Business Needs and Sourcing', @profession),
-  ('Procurement', @profession),
-  ('Contract and Supplier Management', @profession),
-  ('Category Management', @profession);
-  
-SELECT id INTO @parent FROM job_role WHERE name = 'Strategy and Policy Development';
+  ('Strategy and Policy Development', SELECT id FROM profession WHERE name = 'Commercial'),
+  ('Business Needs and Sourcing', SELECT id FROM profession WHERE name = 'Commercial'),
+  ('Procurement', SELECT id FROM profession WHERE name = 'Commercial'),
+  ('Contract and Supplier Management', SELECT id FROM profession WHERE name = 'Commercial'),
+  ('Category Management', SELECT id FROM profession WHERE name = 'Commercial');
 
 INSERT INTO job_role (name, parent_id) VALUES
-  ('Commercial Strategy', @parent),
-  ('Market Maker & Supplier Engagement', @parent),
-  ('Commercial Risk and Assurance Specialist', @parent),
-  ('Commerical Policy Advisor', @parent);
-  
-SELECT id INTO @parent FROM job_role WHERE name = 'Commercial Strategy';
-  
-INSERT INTO job_role (name, parent_id) VALUES
-  ('Commercial Support', @parent),
-  ('Associate Commercial Practitioner', @parent),
-  ('Commercial Practitioner', @parent),
-  ('Commercial Lead', @parent),
-  ('Associate Commercial Specialist', @parent),
-  ('Commercial Specialist', @parent),
-  ('Senior Commercial Specialist', @parent);
+  ('Commercial Strategy', SELECT id FROM job_role WHERE name = 'Strategy and Policy Development'),
+  ('Market Maker & Supplier Engagement', SELECT id FROM job_role WHERE name = 'Strategy and Policy Development'),
+  ('Commercial Risk and Assurance Specialist', SELECT id FROM job_role WHERE name = 'Strategy and Policy Development'),
+  ('Commerical Policy Advisor', SELECT id FROM job_role WHERE name = 'Strategy and Policy Development');
 
+INSERT INTO job_role (name, parent_id) VALUES
+  ('Commercial Support', SELECT id FROM job_role WHERE name = 'Commercial Strategy'),
+  ('Associate Commercial Practitioner', SELECT id FROM job_role WHERE name = 'Commercial Strategy'),
+  ('Commercial Practitioner', SELECT id FROM job_role WHERE name = 'Commercial Strategy'),
+  ('Commercial Lead', SELECT id FROM job_role WHERE name = 'Commercial Strategy'),
+  ('Associate Commercial Specialist', SELECT id FROM job_role WHERE name = 'Commercial Strategy'),
+  ('Commercial Specialist', SELECT id FROM job_role WHERE name = 'Commercial Strategy'),
+  ('Senior Commercial Specialist', SELECT id FROM job_role WHERE name = 'Commercial Strategy');
+
+/*
+21/05 MATT - adding identity and civil_servant inserts for local development of the notifications service.
+As we are lazy loading Identities and Civil Servants into CSRS, this creates problems trying to notify users who have never signed-up/logged into LPG
+These inserts probably shouldn't be run in any of the environments.
+*/
+INSERT INTO `identity` (uid) VALUES
+('3c706a70-3fff-4e7b-ae7f-102c1d46f569'), ('8dc80f78-9a52-4c31-ac54-d280a70c18eb');
+
+INSERT INTO `civil_servant` (identity_id, organisation_id, grade_id, profession_id, job_role_id, full_name) VALUES
+(SELECT id from identity where uid = '3c706a70-3fff-4e7b-ae7f-102c1d46f569',
+SELECT id from organisation where code = 'co',
+SELECT id from grade where code = 'G7',
+SELECT id from profession where name = 'Commercial',
+SELECT id from job_role where name = 'Commercial Support',
+'Learner'
+),
+(SELECT id from identity where uid = '8dc80f78-9a52-4c31-ac54-d280a70c18eb',
+SELECT id from organisation where code = 'co',
+SELECT id from grade where code = 'G6',
+SELECT id from profession where name = 'Communications',
+SELECT id from job_role where name = 'Commercial Strategy',
+'Super'
+)
+;
 SET FOREIGN_KEY_CHECKS = 1;
-
