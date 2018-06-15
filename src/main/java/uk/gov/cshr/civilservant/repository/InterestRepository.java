3515ee95fc9c741fe0f6e9fadb031ab4f017b1b4
alan <alan@kainos.com>
2018-06-15 17:42:02 +0100

    utility

--- src/main/java/uk/gov/cshr/civilservant/repository/InterestRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/InterestRepository.java
@@ -3,10 +3,10 @@ package uk.gov.cshr.civilservant.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
-import uk.gov.cshr.civilservant.domain.Profession;
+import uk.gov.cshr.civilservant.domain.Interest;
 
 @Repository
 @RepositoryRestResource
-public interface InterestRepository extends CrudRepository<Profession, Long> {
+public interface InterestRepository extends CrudRepository<Interest, Long> {
 
 }
