b3595e6d43f59f5627c4e1d5e6a63d4795eb6aa8
alan <alan.work@teamsmog.com>
2018-06-18 11:45:24 +0100

    data migration

--- src/main/resources/db/migration/mysql/V1.3__init.sql
+++ src/main/resources/db/migration/mysql/V1.3__init.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `interest` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `civil_servant_interests` (
-  `civil_servant_id` mediumint(8) NOT NULL,
-  `interests_id` smallint(5) NOT NULL,
+  `civil_servant_id` mediumint(8) unsigned NOT NULL,
+  `interests_id` smallint(6) unsigned NOT NULL,
   PRIMARY KEY (`civil_servant_id`,`interests_id`),
   CONSTRAINT `FK_civil_servant_interests_civil_servant` FOREIGN KEY (`civil_servant_id`) REFERENCES `civil_servant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_civil_servant_interests_interest` FOREIGN KEY (`interests_id`) REFERENCES `interest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
