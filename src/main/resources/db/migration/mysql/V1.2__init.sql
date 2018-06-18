b1869f99069c4422da91d488f990da2a5e59fad9
alan <alan.work@teamsmog.com>
2018-06-18 10:33:56 +0100

    updated sql structure

--- src/main/resources/db/migration/mysql/V1.2__init.sql
+++ src/main/resources/db/migration/mysql/V1.2__init.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS `interest` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `civil_servant_interests` (
+  `civil_servant_id` mediumint(8) NOT NULL,
+  `interests_id` smallint(5) NOT NULL,
+  PRIMARY KEY (`civil_servant_id`,`interests_id`),
+  CONSTRAINT `FK_civil_servant_interests_civil_servant` FOREIGN KEY (`civil_servant_id`) REFERENCES `civil_servant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_civil_servant_interests_interest` FOREIGN KEY (`interests_id`) REFERENCES `interest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
