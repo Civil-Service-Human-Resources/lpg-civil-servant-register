63716763932695e81c57a31b28f1a929c5e4d3ce
Max Stewart <max@cautionyourblast.com>
2018-06-25 15:45:22 +0100

    Add search to organisation.

--- src/main/java/uk/gov/cshr/civilservant/repository/OrganisationRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/OrganisationRepository.java
@@ -1,5 +1,6 @@
 package uk.gov.cshr.civilservant.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -7,10 +8,16 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import uk.gov.cshr.civilservant.domain.Organisation;
 
+import java.util.Optional;
+
 @Repository
 @RepositoryRestResource
 public interface OrganisationRepository extends CrudRepository<Organisation, Long> {
 
     @RestResource(path = "name", rel = "searchName")
     Iterable<Organisation> findByNameStartsWithIgnoringCase(@Param("query") String query);
+
+    @RestResource(path = "findByDepartmentCode", rel = "findByDepartmentCode")
+    @Query("SELECT o FROM Organisation o WHERE o.department.code = ?1")
+    Optional<Organisation> findByDepartmentCode(@Param("departmentCode") String code);
 }
