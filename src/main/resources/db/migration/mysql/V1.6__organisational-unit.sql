e05e2794a3701da573a050744fa72bd4033883df
Rob Leney <roble@kainos.com>
2018-10-05 14:18:18 +0100

    LPFG-817: Added database migration to create organisational_unit table and populate with existing data from organisation, department and department_payment_methods tables

---
+++ src/main/resources/db/migration/mysql/V1.6__organisational-unit.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `organisational_unit`;
+
+CREATE TABLE `organisational_unit` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) unsigned DEFAULT NULL,
+  `code` varchar(10) NOT NULL,
+  `abbreviation` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `payment_methods` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_name` (`name`),
+  UNIQUE KEY `unique_code` (`code`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `fk_organisational_unit_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `organisational_unit` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `organisational_unit` (`id`, `code`, `name`, `payment_methods`)
+  SELECT `id`, `code`, `name`, GROUP_CONCAT(`payment_method`) AS `payment_methods`
+  FROM `department`
+    JOIN `department_payment_methods` `dpm`
+      ON `dpm`.`department_id` = `department`.`id`
+  GROUP BY id;
