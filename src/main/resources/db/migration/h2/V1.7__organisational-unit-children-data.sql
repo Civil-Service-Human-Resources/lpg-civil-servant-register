55e71d820c7f4eacc910be8d8e3337f7f1f99e14
matthewmu <matthewmu@kainos.com>
2018-11-08 18:17:14 +0000

    LPFG-936: fix migration issue

--- src/main/resources/db/migration/h2/V1.7__organisational-unit-children-data.sql
+++ src/main/resources/db/migration/h2/V1.7__organisational-unit-children-data.sql
@@ -1,4 +1,4 @@
 INSERT INTO `organisational_unit` (`id`, `parent_id`, `code`, `abbreviation`, `name`) VALUES
 (4, 1, 'D1.1', 'COC1', 'Cabinet Office Child 1'),
-(5, 1, 'D1.2', 'COC2', 'Cabinet Office Child 1'),
+(5, 1, 'D1.2', 'COC2', 'Cabinet Office Child 2'),
 (6, 5, 'D1.2.1', 'COGC1', 'Cabinet Office Grandchild 1');
