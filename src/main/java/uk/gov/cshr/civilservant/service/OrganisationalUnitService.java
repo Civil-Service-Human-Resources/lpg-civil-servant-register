d5c49b47b842915c8fd4040977cbf9d941e29401
matthewmu <matthewmu@kainos.com>
2018-11-12 12:40:02 +0000

    LPFG-936: updating parent structure, refactoring, tests

--- src/main/java/uk/gov/cshr/civilservant/service/OrganisationalUnitService.java
+++ src/main/java/uk/gov/cshr/civilservant/service/OrganisationalUnitService.java
@@ -1,16 +1,43 @@
 package uk.gov.cshr.civilservant.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.cshr.civilservant.domain.OrganisationalUnit;
+import uk.gov.cshr.civilservant.repository.OrganisationalUnitRepository;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Service
 public class OrganisationalUnitService {
 
-    public Map<String, String> getOrganisationalUnitsMap(Iterable<OrganisationalUnit> organisationalUnits) {
+    OrganisationalUnitRepository organisationalUnitRepository;
+
+    @Autowired
+    public OrganisationalUnitService(OrganisationalUnitRepository organisationalUnitRepository) {
+        this.organisationalUnitRepository = organisationalUnitRepository;
+    }
+
+    public ArrayList<OrganisationalUnit> getParentOrganisationalUnits() {
+        ArrayList<OrganisationalUnit> organisationalUnitArrayList = new ArrayList<>();
+
+        Iterable<OrganisationalUnit> organisationalUnits = organisationalUnitRepository.findAll();
+
+        organisationalUnits.forEach(organisationalUnit -> {
+            OrganisationalUnit currentNode = organisationalUnit;
+            if (!currentNode.hasParent()) {
+                organisationalUnitArrayList.add(currentNode);
+            }
+        });
+
+        return organisationalUnitArrayList;
+    }
+
+    public Map<String, String> getOrganisationalUnitsMap() {
         Map<String, String> organisationalUnitsMap = new LinkedHashMap<>();
+
+        Iterable<OrganisationalUnit> organisationalUnits = organisationalUnitRepository.findAll();
 
         organisationalUnits.forEach(organisationalUnit -> {
             OrganisationalUnit currentNode = organisationalUnit;
--- src/test/java/uk/gov/cshr/civilservant/service/OrganisationalUnitServiceTest.java
+++ src/test/java/uk/gov/cshr/civilservant/service/OrganisationalUnitServiceTest.java
@@ -2,27 +2,65 @@ package uk.gov.cshr.civilservant.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import uk.gov.cshr.civilservant.domain.OrganisationalUnit;
+import uk.gov.cshr.civilservant.repository.OrganisationalUnitRepository;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class OrganisationalUnitServiceTest {
 
     private OrganisationalUnitService organisationalUnitService;
 
+    @Mock
+    private OrganisationalUnitRepository organisationalUnitRepository;
+
     @Before
     public void setup() {
         initMocks(this);
-        organisationalUnitService = new OrganisationalUnitService();
+        organisationalUnitService = new OrganisationalUnitService(organisationalUnitRepository);
+    }
+
+    @Test
+    public void shouldReturnParentOrganisationalUnits() {
+        ArrayList<OrganisationalUnit> organisationalUnits = createOrganisationalUnitStructure();
+
+        when(organisationalUnitRepository.findAll()).thenReturn(organisationalUnits);
+
+        ArrayList<OrganisationalUnit> parentOrganisationalUnits = organisationalUnitService.getParentOrganisationalUnits();
+
+        assertThat(parentOrganisationalUnits.size(), equalTo(2));
+
+        assertThat(parentOrganisationalUnits, containsInAnyOrder(
+                hasProperty("code", equalTo("PT1")),
+                hasProperty("code", equalTo("PT2"))
+        ));
     }
 
     @Test
     public void shouldReturnOrganisationalUnitsAsMap() {
+        ArrayList<OrganisationalUnit> organisationalUnits = createOrganisationalUnitStructure();
+
+        when(organisationalUnitRepository.findAll()).thenReturn(organisationalUnits);
+
+        Map<String, String> organisationalUnitsMap = organisationalUnitService.getOrganisationalUnitsMap();
+
+        assertThat(organisationalUnitsMap.size(), equalTo(4));
+        assertThat(organisationalUnitsMap.get("PT1"), equalTo("ParentTest1 (PT1)"));
+        assertThat(organisationalUnitsMap.get("PT2"), equalTo("ParentTest2 (PT2)"));
+        assertThat(organisationalUnitsMap.get("CT1"), equalTo("ParentTest1 (PT1), ChildTest1 (CT1)"));
+        assertThat(organisationalUnitsMap.get("GCT1"), equalTo("ParentTest1 (PT1), ChildTest1 (CT1), GrandchildTest1 (GCT1)"));
+    }
+
+    private ArrayList<OrganisationalUnit> createOrganisationalUnitStructure() {
         OrganisationalUnit parentOrganisationalUnit1 = new OrganisationalUnit();
         parentOrganisationalUnit1.setName("ParentTest1");
         parentOrganisationalUnit1.setCode("PT1");
@@ -39,17 +77,18 @@ public class OrganisationalUnitServiceTest {
         childOrganisationalUnit.setAbbreviation("CT1");
         childOrganisationalUnit.setParent(parentOrganisationalUnit1);
 
+        OrganisationalUnit grandchildOrganisationalUnit = new OrganisationalUnit();
+        grandchildOrganisationalUnit.setName("GrandchildTest1");
+        grandchildOrganisationalUnit.setCode("GCT1");
+        grandchildOrganisationalUnit.setAbbreviation("GCT1");
+        grandchildOrganisationalUnit.setParent(childOrganisationalUnit);
+
         ArrayList<OrganisationalUnit> organisationalUnits = new ArrayList<>();
         organisationalUnits.add(parentOrganisationalUnit1);
         organisationalUnits.add(parentOrganisationalUnit2);
         organisationalUnits.add(childOrganisationalUnit);
+        organisationalUnits.add(grandchildOrganisationalUnit);
 
-        Map<String, String> organisationalUnitsMap = organisationalUnitService.getOrganisationalUnitsMap(organisationalUnits);
-
-        assertThat(organisationalUnitsMap.size(), equalTo(3));
-        assertThat(organisationalUnitsMap.get(parentOrganisationalUnit1.getCode()), equalTo("ParentTest1 (PT1)"));
-        assertThat(organisationalUnitsMap.get(parentOrganisationalUnit2.getCode()), equalTo("ParentTest2 (PT2)"));
-        assertThat(organisationalUnitsMap.get(childOrganisationalUnit.getCode()), equalTo("ParentTest1 (PT1), ChildTest1 (CT1)"));
+        return organisationalUnits;
     }
-
 }
