0dc18b2680b62f875b2486e362953d29fdd6c7b1
matthewmu <matthewmu@kainos.com>
2018-11-23 17:50:21 +0000

    LPFG-929: add config to expose id when creating new org unit

---
+++ src/main/java/uk/gov/cshr/civilservant/config/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package uk.gov.cshr.civilservant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import uk.gov.cshr.civilservant.domain.OrganisationalUnit;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(OrganisationalUnit.class);
+    }
+}
