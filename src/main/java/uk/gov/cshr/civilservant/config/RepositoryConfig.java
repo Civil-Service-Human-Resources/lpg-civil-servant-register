aaec8da35ad283d62f0be4ed04af94672a65729a
Rob Leney <roble@kainos.com>
2018-11-30 14:25:23 +0000

    LPFG-1037: Expose ids on professions. (I really mean it this time)

--- src/main/java/uk/gov/cshr/civilservant/config/RepositoryConfig.java
+++ src/main/java/uk/gov/cshr/civilservant/config/RepositoryConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import uk.gov.cshr.civilservant.domain.OrganisationalUnit;
+import uk.gov.cshr.civilservant.domain.Profession;
 
 @Configuration
 public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
@@ -11,5 +12,6 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(OrganisationalUnit.class);
+        config.exposeIdsFor(Profession.class);
     }
 }
