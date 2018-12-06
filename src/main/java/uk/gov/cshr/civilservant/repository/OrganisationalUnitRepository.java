f82689cf87241f100c067a20004c4cbdeb0e0309
matthewmu <matthewmu@kainos.com>
2018-12-06 13:18:29 +0000

    tests

--- src/main/java/uk/gov/cshr/civilservant/repository/OrganisationalUnitRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/OrganisationalUnitRepository.java
@@ -2,15 +2,12 @@ package uk.gov.cshr.civilservant.repository;
 
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 import uk.gov.cshr.civilservant.domain.OrganisationalUnit;
 
 @Repository
 @RepositoryRestResource
 public interface OrganisationalUnitRepository extends SelfReferencingEntityRepository<OrganisationalUnit> {
-    OrganisationalUnit findByCode(@Param("code") String code);
 
-    @Override
-    OrganisationalUnit save(OrganisationalUnit organisationalUnit);
+    OrganisationalUnit findByCode(@Param("code") String code);
 }
--- src/test/java/uk/gov/cshr/civilservant/controller/OrganisationalUnitControllerTest.java
+++ src/test/java/uk/gov/cshr/civilservant/controller/OrganisationalUnitControllerTest.java
@@ -1,14 +1,11 @@
 package uk.gov.cshr.civilservant.controller;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.gson.GsonBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,12 +16,9 @@ import uk.gov.cshr.civilservant.service.OrganisationalUnitService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -64,34 +58,5 @@ public class OrganisationalUnitControllerTest {
                         .accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
-    }
-
-    @Test
-    public void shouldNotSaveOrganisationIfNotOrganisationManager() throws Exception {
-        Map<String, String> organisation = ImmutableMap.of("name", "new profession");
-
-        String json = new GsonBuilder().create().toJson(organisation);
-
-        mockMvc.perform(
-                post("/organisationalUnits").with(csrf())
-                        .content(json)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithMockUser(username = "user", roles = "ORGANISATION_MANAGER")
-    public void shouldSaveOrganisationIfOrganisationManager() throws Exception {
-        Map<String, String> organisation = ImmutableMap.of("name", "foo", "code", "bar");
-
-        String json = new GsonBuilder().create().toJson(organisation);
-
-        mockMvc.perform(
-                post("/organisationalUnits").with(csrf())
-                        .content(json)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isCreated());
     }
 }
