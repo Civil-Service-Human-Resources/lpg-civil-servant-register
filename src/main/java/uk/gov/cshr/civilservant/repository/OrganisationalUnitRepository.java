20a04068bd5dcd2b4bc53e63122d95ddc51d8c36
matthewmu <matthewmu@kainos.com>
2018-12-06 13:09:24 +0000

    permissions

--- src/main/java/uk/gov/cshr/civilservant/repository/OrganisationalUnitRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/OrganisationalUnitRepository.java
@@ -11,7 +11,6 @@ import uk.gov.cshr.civilservant.domain.OrganisationalUnit;
 public interface OrganisationalUnitRepository extends SelfReferencingEntityRepository<OrganisationalUnit> {
     OrganisationalUnit findByCode(@Param("code") String code);
 
-    @PreAuthorize("hasRole('ORGANISATION_MANAGER')")
     @Override
     OrganisationalUnit save(OrganisationalUnit organisationalUnit);
 }
