db4774b560c965464eda93276a1b47844dba2c6e
Rob Leney <roble@kainos.com>
2018-12-19 08:54:46 +0000

    LPFG-509: Removed unused imports

--- src/main/java/uk/gov/cshr/civilservant/Application.java
+++ src/main/java/uk/gov/cshr/civilservant/Application.java
@@ -2,7 +2,6 @@ package uk.gov.cshr.civilservant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 /**
