b6b98f92d270c3964ee36fefa842f9969311bd4d
Peter Taylor <petertaylor@Malekith.local>
2019-02-06 16:31:28 +0000

    LPFG-1255: flat endpoint

--- src/main/java/uk/gov/cshr/civilservant/controller/ProfessionController.java
+++ src/main/java/uk/gov/cshr/civilservant/controller/ProfessionController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.cshr.civilservant.domain.Profession;
+import uk.gov.cshr.civilservant.dto.OrganisationalUnitDto;
+import uk.gov.cshr.civilservant.dto.ProfessionDto;
 import uk.gov.cshr.civilservant.service.ProfessionService;
 
 import java.util.List;
@@ -21,6 +23,13 @@ public class ProfessionController {
     @GetMapping("/tree")
     public ResponseEntity<List<Profession>> listProfessionsAsTreeStructure() {
         List<Profession> professions = professionService.getParents();
+
+        return ResponseEntity.ok(professions);
+    }
+
+    @GetMapping("/flat")
+    public ResponseEntity<List<ProfessionDto>> listOrganisationalUnitsAsFlatStructure() {
+        List<ProfessionDto> professions = professionService.getListSortedByValue();
 
         return ResponseEntity.ok(professions);
     }
