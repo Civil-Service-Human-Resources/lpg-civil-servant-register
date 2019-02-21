a341b8c365f16bf0c255f70c6c823c2cea1e2898
Peter Taylor <petertaylor@Malekith.local>
2019-02-21 16:22:31 +0000

    LPFG-1255: Re kick off build and deploy

--- src/main/java/uk/gov/cshr/civilservant/controller/ProfessionController.java
+++ src/main/java/uk/gov/cshr/civilservant/controller/ProfessionController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.cshr.civilservant.domain.Profession;
-import uk.gov.cshr.civilservant.dto.OrganisationalUnitDto;
 import uk.gov.cshr.civilservant.dto.ProfessionDto;
 import uk.gov.cshr.civilservant.service.ProfessionService;
 
