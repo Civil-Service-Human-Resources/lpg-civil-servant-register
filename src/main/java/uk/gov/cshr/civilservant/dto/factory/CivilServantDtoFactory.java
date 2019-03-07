1e8ec9600d04a89c40cb4a3a633bd66164a8a667
matthewmu <matthewmu@kainos.com>
2019-03-07 11:57:28 +0000

    LC-164: removing id call fully

--- src/main/java/uk/gov/cshr/civilservant/dto/factory/CivilServantDtoFactory.java
+++ src/main/java/uk/gov/cshr/civilservant/dto/factory/CivilServantDtoFactory.java
@@ -4,23 +4,19 @@ import org.springframework.stereotype.Component;
 import uk.gov.cshr.civilservant.domain.CivilServant;
 import uk.gov.cshr.civilservant.domain.Profession;
 import uk.gov.cshr.civilservant.dto.CivilServantDto;
-import uk.gov.cshr.civilservant.service.identity.IdentityService;
 
 import java.util.stream.Collectors;
 
 @Component
 public class CivilServantDtoFactory {
 
-    private final IdentityService identityService;
+    public CivilServantDtoFactory() {
 
-    public CivilServantDtoFactory(IdentityService identityService) {
-        this.identityService = identityService;
     }
 
     public CivilServantDto create(CivilServant civilServant) {
         CivilServantDto civilServantDto = new CivilServantDto();
         civilServantDto.setId(civilServant.getIdentity().getUid());
-        civilServantDto.setEmail("test@example.com");
         civilServantDto.setName(civilServant.getFullName());
 
         if (civilServant.getOrganisationalUnit().isPresent()) {
