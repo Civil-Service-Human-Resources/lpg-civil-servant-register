192062d076c282cd025ac9fa564043e36ee86bbf
matthewmu <matthewmu@kainos.com>
2019-06-11 14:10:14 +0100

    LC-227: handling modules with no modules on home screen

--- src/test/java/uk/gov/cshr/civilservant/repository/ProfessionRepositoryTest.java
+++ src/test/java/uk/gov/cshr/civilservant/repository/ProfessionRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.cshr.civilservant.domain.Profession;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -43,28 +42,5 @@ public class ProfessionRepositoryTest {
         assertTrue(optional.isPresent());
 
         assertEquals(parent, optional.get().getParent());
-    }
-
-    @Test
-    public void shouldReturnProfessionAndChildren() {
-        Profession parent = new Profession();
-        parent.setName("parent");
-
-        Profession child1 = new Profession();
-        child1.setName("child1");
-
-        Profession child2 = new Profession();
-        child2.setName("child2");
-
-        parent.setChildren(Arrays.asList(child1, child2));
-
-        Profession savedParent = professionRepository.save(parent);
-
-        Optional<Profession> optional = professionRepository.findById(savedParent.getId());
-
-        assertTrue(optional.isPresent());
-        assertEquals(Arrays.asList(child1, child2), savedParent.getChildren());
-
-        assertTrue(professionRepository.findById(savedParent.getChildren().get(0).getId()).isPresent());
     }
 }
