dfebe96ac954f3117679b0b0c3640c1ff274e399
matthewmu <matthewmu@kainos.com>
2019-07-10 09:55:10 +0100

    LC-14: adding local data

--- src/main/resources/db/migration/h2/V1.12__agency.sql
+++ src/main/resources/db/migration/h2/V1.12__agency.sql
@@ -1,18 +1,24 @@
 CREATE TABLE IF NOT EXISTS `agency_token`
 (
-    `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id`    SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
     `token` VARCHAR (10) NOT NULL UNIQUE,
-    `domain`  VARCHAR(150) NOT NULL,
-    `usage` SMALLINT(5) NOT NULL,
-    `capacity` SMALLINT(5) NOT NULL,
-    `organisational_unit_id` SMALLINT(5) DEFAULT NULL,
-    PRIMARY KEY (`id`),
+    `domain`  char(20) NOT NULL,
+    `tokens_used` smallint(5) NOT NULL,
+    `capacity` smallint(5) NOT NULL,
+    `organisational_unit_id` smallint(5) unsigned,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 SET FOREIGN_KEY_CHECKS=0;
 
 ALTER TABLE `agency_token`
-ADD CONSTRAINT `FK_civil_servant_organisational_unit_id` FOREIGN KEY (`organisational_unit_id`) REFERENCES `organisational_unit` (`id`);
+ADD CONSTRAINT `FK_agency_organisational_unit_id` FOREIGN KEY (`organisational_unit_id`) REFERENCES `organisational_unit` (`id`) on DELETE CASCADE;
 
 SET FOREIGN_KEY_CHECKS=1;
+
+INSERT INTO agency_token values
+(1, 'token123', 'domain.com', 100, 10, 1),
+(2, 'token2', 'example.com', 200, 15, 1),
+(3, 'token3', 'kainos.com', 300, 0, 1)
+;
