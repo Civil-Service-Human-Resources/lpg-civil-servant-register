84586647e79e2d78dc6c84d2fc078196c62df8b8
matthewmu <matthewmu@kainos.com>
2019-07-10 16:52:40 +0100

    LC-14: db updates

--- src/main/resources/db/migration/h2/V1.12__agency.sql
+++ src/main/resources/db/migration/h2/V1.12__agency.sql
@@ -5,20 +5,14 @@ CREATE TABLE IF NOT EXISTS `agency_token`
     `domain`  char(20) NOT NULL,
     `tokens_used` smallint(5) NOT NULL,
     `capacity` smallint(5) NOT NULL,
-    `organisational_unit_id` smallint(5) unsigned,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE `agency_token`
-ADD CONSTRAINT `FK_agency_organisational_unit_id` FOREIGN KEY (`organisational_unit_id`) REFERENCES `organisational_unit` (`id`) on DELETE CASCADE;
+ALTER TABLE `organisational_unit`
+ADD COLUMN agency_token_id SMALLINT(5) UNSIGNED,
+ADD CONSTRAINT `FK_organisational_unit_agency_token` FOREIGN KEY (`agency_token_id`) REFERENCES `agency_token` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 SET FOREIGN_KEY_CHECKS=1;
-
-INSERT INTO agency_token values
-(1, 'token123', 'domain.com', 100, 10, 1),
-(2, 'token2', 'example.com', 200, 15, 1),
-(3, 'token3', 'kainos.com', 300, 0, 1)
-;
