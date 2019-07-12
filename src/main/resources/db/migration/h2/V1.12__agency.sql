eed13e4849ed8e634de17731d4ac9c8d2a53cd0d
matthewmu <matthewmu@kainos.com>
2019-07-12 15:39:06 +0100

    LC-14: db updates

--- src/main/resources/db/migration/h2/V1.12__agency.sql
+++ src/main/resources/db/migration/h2/V1.12__agency.sql
@@ -1,11 +1,29 @@
-CREATE TABLE IF NOT EXISTS `agency_token`
+CREATE TABLE IF NOT EXISTS `agency_domain`
+(
+    `id`    SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `domain` VARCHAR(150) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+  CREATE TABLE IF NOT EXISTS `agency_token`
 (
     `id`    SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
     `token` VARCHAR (10) NOT NULL UNIQUE,
-    `domain`  char(20) NOT NULL,
-    `tokens_used` smallint(5) NOT NULL,
     `capacity` smallint(5) NOT NULL,
     PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+  CREATE TABLE `agency_token_agency_domains`
+(
+    `agency_token_id` SMALLINT(5) UNSIGNED NOT NULL,
+    `agency_domains_id`  SMALLINT(5) UNSIGNED NOT NULL,
+    PRIMARY KEY (`agency_token_id`, `agency_domains_id`),
+    CONSTRAINT `FK_token_domain_token` FOREIGN KEY (`agency_token_id`) REFERENCES `agency_token` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `FK_token_domain_domains` FOREIGN KEY (`agency_domains_id`) REFERENCES `agency_domain` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -19,5 +37,15 @@ ADD CONSTRAINT `FK_organisational_unit_agency_token` FOREIGN KEY (`agency_token_
 
 SET FOREIGN_KEY_CHECKS=1;
 
-INSERT INTO `agency_token` values (1, 'token', 'domain.com', 10, 100);
+INSERT INTO agency_token VALUES (1, 'token123', 100);
+INSERT INTO agency_token VALUES (2, 'token456', 200);
+INSERT INTO agency_domain VALUES (1, 'domain.com');
+INSERT INTO agency_domain VALUES (2, 'example.com');
+INSERT INTO agency_domain VALUES (3, 'test.com');
+
+INSERT INTO agency_token_agency_domains VALUES (1, 1);
+INSERT INTO agency_token_agency_domains VALUES (1, 2);
+INSERT INTO agency_token_agency_domains VALUES (2, 3);
+
 UPDATE `organisational_unit` SET agency_token_id = 1 WHERE id = 1;
+UPDATE `organisational_unit` SET agency_token_id = 2 WHERE id = 2;
