88297fc5b507fe99f15f10776e0e171eca60ac29
matthewmu <matthewmu@kainos.com>
2019-07-12 15:39:06 +0100

    LC-14: db updates

--- src/main/resources/db/migration/mysql/V1.13__agency.sql
+++ src/main/resources/db/migration/mysql/V1.13__agency.sql
@@ -1,11 +1,29 @@
-CREATE TABLE IF NOT EXISTS `agency_token`
+CREATE TABLE IF NOT EXISTS `agency_domain`
+(
+    `id`    SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `domain` VARCHAR(150) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+  CREATE TABLE IF NOT EXISTS `agency_token`
 (
     `id`    SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
     `token` VARCHAR (10) NOT NULL UNIQUE,
-    `domain`  char(20) NOT NULL,
-    `tokens_used` smallint(5) NOT NULL,
     `capacity` smallint(5) NOT NULL,
     PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+  CREATE TABLE `agency_token_agency_domains`
+(
+    `agency_token_id` SMALLINT(5) UNSIGNED NOT NULL,
+    `agency_domains_id`  SMALLINT(5) UNSIGNED NOT NULL,
+    PRIMARY KEY (`agency_token_id`, `agency_domains_id`),
+    CONSTRAINT `FK_token_domain_token` FOREIGN KEY (`agency_token_id`) REFERENCES `agency_token` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `FK_token_domain_domains` FOREIGN KEY (`agency_domains_id`) REFERENCES `agency_domain` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
