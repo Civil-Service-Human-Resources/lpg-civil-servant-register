43395b62c78e512499ca316d4f0541337ef4f2d9
matthewmu <matthewmu@kainos.com>
2019-07-25 13:06:51 +0100

    LC-244: initial caching

--- src/main/java/uk/gov/cshr/civilservant/Application.java
+++ src/main/java/uk/gov/cshr/civilservant/Application.java
@@ -2,6 +2,7 @@ package uk.gov.cshr.civilservant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
  */
 @SpringBootApplication
 @EnableSpringConfigured
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
