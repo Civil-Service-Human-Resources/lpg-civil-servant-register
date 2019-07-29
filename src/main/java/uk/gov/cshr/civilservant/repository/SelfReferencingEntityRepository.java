b374a843eef6d01d3dfe71379fc9431738480e31
matthewmu <matthewmu@kainos.com>
2019-07-29 11:09:12 +0100

    LC-244: updating delete and update

--- src/main/java/uk/gov/cshr/civilservant/repository/SelfReferencingEntityRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/SelfReferencingEntityRepository.java
@@ -3,6 +3,7 @@ package uk.gov.cshr.civilservant.repository;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.security.access.prepost.PreAuthorize;
 import uk.gov.cshr.civilservant.domain.SelfReferencingEntity;
 
 import java.util.List;
@@ -14,4 +15,9 @@ public interface SelfReferencingEntityRepository<T extends SelfReferencingEntity
     @Override
     @CacheEvict(value = {"organisationalUnitsTree", "organisationalUnitsFlat"}, allEntries = true)
     <S extends T> S save(S entity);
+
+    @Override
+    @CacheEvict(value = {"organisationalUnitsTree", "organisationalUnitsFlat"}, allEntries = true)
+    @PreAuthorize("isAuthenticated()")
+    void deleteById(Long aLong);
 }
