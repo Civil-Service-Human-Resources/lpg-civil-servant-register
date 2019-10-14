d967faac8a093a66749827e98a133eed346e2764
bridgeth <b.hodgson@kainos.com>
2019-10-14 18:31:30 +0100

    LC-14 - Added unit tests

--- src/main/java/uk/gov/cshr/civilservant/dto/AgencyTokenDTO.java
+++ src/main/java/uk/gov/cshr/civilservant/dto/AgencyTokenDTO.java
@@ -2,7 +2,9 @@ package uk.gov.cshr.civilservant.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class AgencyTokenDTO {
