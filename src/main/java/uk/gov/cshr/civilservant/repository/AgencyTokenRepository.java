105cb38ac1a2d1349ee8738d0e47715515d4ef13
bridgeth <b.hodgson@kainos.com>
2019-10-15 13:56:37 +0100

    LC-14 - Added id to resolve update

--- src/main/java/uk/gov/cshr/civilservant/repository/AgencyTokenRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/AgencyTokenRepository.java
@@ -11,20 +11,20 @@ import java.util.Optional;
 @Repository
 public interface AgencyTokenRepository extends CrudRepository<AgencyToken, Long> {
 
-    @Query("select new uk.gov.cshr.civilservant.domain.AgencyToken(a.token, a.capacity, a.capacityUsed) " +
+    @Query("select new uk.gov.cshr.civilservant.domain.AgencyToken(a.id, a.token, a.capacity, a.capacityUsed) " +
             "from AgencyToken a " +
             "left join a.agencyDomains d " +
             "where d.domain = ?1 ")
     List<AgencyToken> findAllByDomain(String domain);
 
-    @Query("select new uk.gov.cshr.civilservant.domain.AgencyToken(a.token, a.capacity, a.capacityUsed) " +
+    @Query("select new uk.gov.cshr.civilservant.domain.AgencyToken(a.id, a.token, a.capacity, a.capacityUsed) " +
             "from AgencyToken a " +
             "left join a.agencyDomains d " +
             "where d.domain = ?1 " +
             "and a.token = ?2")
     Optional<AgencyToken> findByDomainAndToken(String domain, String token);
 
-    @Query("select new uk.gov.cshr.civilservant.domain.AgencyToken(a.token, a.capacity, a.capacityUsed) " +
+    @Query("select new uk.gov.cshr.civilservant.domain.AgencyToken(a.id, a.token, a.capacity, a.capacityUsed) " +
             "from AgencyToken a " +
             "left join a.agencyDomains d " +
             "left join OrganisationalUnit ou on ou.agencyToken.id = a.id  " +
