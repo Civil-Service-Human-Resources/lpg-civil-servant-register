3c706f1aa1dfc0e74bbb89bf80a4b9be1e8c5115
charlesluo_div <charlesl@kainos.com>
2019-10-25 16:29:58 +0100

    add Azure App Insights

--- src/test/java/uk/gov/cshr/civilservant/controller/CivilServantControllerTest.java
+++ src/test/java/uk/gov/cshr/civilservant/controller/CivilServantControllerTest.java
@@ -41,7 +41,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
