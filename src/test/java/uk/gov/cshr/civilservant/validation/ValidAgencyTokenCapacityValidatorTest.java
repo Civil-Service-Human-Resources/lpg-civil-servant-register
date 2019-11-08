5dffaf7838a3c1bdc69b81211713b4d8a9b7124f
bridgeth <b.hodgson@kainos.com>
2019-11-08 16:46:42 +0000

    LC-14 - Refactored to be one validator for both min and max capacity on AgencyToken

--- src/test/java/uk/gov/cshr/civilservant/validation/ValidAgencyTokenCapacityValidatorTest.java
+++ src/test/java/uk/gov/cshr/civilservant/validation/ValidAgencyTokenCapacityValidatorTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
