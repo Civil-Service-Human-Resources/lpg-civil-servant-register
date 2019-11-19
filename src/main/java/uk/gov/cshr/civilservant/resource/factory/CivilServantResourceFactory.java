cb6711b7f4d546c2f24db60c39ebc5a6d6a52950
bridgeth <b.hodgson@kainos.com>
2019-11-19 17:38:37 +0000

    LC-14 - Added unit tests for new get org code for civil servant.  Made method return Optional.

--- src/main/java/uk/gov/cshr/civilservant/resource/factory/CivilServantResourceFactory.java
+++ src/main/java/uk/gov/cshr/civilservant/resource/factory/CivilServantResourceFactory.java
@@ -3,8 +3,11 @@ package uk.gov.cshr.civilservant.resource.factory;
 import org.springframework.hateoas.Resource;
 import org.springframework.stereotype.Component;
 import uk.gov.cshr.civilservant.domain.CivilServant;
+import uk.gov.cshr.civilservant.dto.OrgCodeDTO;
 import uk.gov.cshr.civilservant.resource.CivilServantResource;
 import uk.gov.cshr.civilservant.service.identity.IdentityService;
+
+import java.util.Optional;
 
 @Component
 public class CivilServantResourceFactory {
@@ -54,11 +57,13 @@ public class CivilServantResourceFactory {
         return resource;
     }
 
-    public String getCivilServantOrganisationalUnitCode(CivilServant civilServant) {
+    public Optional<OrgCodeDTO> getCivilServantOrganisationalUnitCode(CivilServant civilServant) {
         if(civilServant.getOrganisationalUnit().isPresent() && civilServant.getOrganisationalUnit().get().getCode() != null){
-            return civilServant.getOrganisationalUnit().get().getCode();
+            OrgCodeDTO dto = new OrgCodeDTO();
+            dto.setCode(civilServant.getOrganisationalUnit().get().getCode());
+            return Optional.of(dto);
         } else {
-            return "";
+            return Optional.empty();
         }
     }
 
