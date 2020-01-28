98beefa970dccfc3ee34efa503be31502f8ca762
bridgeth <b.hodgson@kainos.com>
2020-01-28 11:04:48 +0000

    LC-329 - Added unit tests for getParents method

--- src/test/java/uk/gov/cshr/civilservant/service/OrganisationalUnitServiceTest.java
+++ src/test/java/uk/gov/cshr/civilservant/service/OrganisationalUnitServiceTest.java
@@ -33,94 +33,19 @@ public class OrganisationalUnitServiceTest {
     private static int COUNTER = 0;
 
     private static String GODFATHERS_CODE = "gf";
-
+    ould contain
+extracting((Or
+    only these
     @Mock
     private OrganisationalUnitRepository organisationalUnitRepository;
-
     @Mock
     private OrganisationalUnitDtoFactory organisationalUnitDtoFactory;
-
     @Mock
     private AgencyTokenService agencyTokenService;
-
     @InjectMocks
     private OrganisationalUnitService organisationalUnitService;
-    private
-    c List
 
-
-    <OrganisationalUnit> buildLargeFamilyOfOrganisationalUnits() {
-        // the family entirely
-        List<OrganisationalUnit> theFamily = new ArrayList<>();
-
-        OrganisationalUnit headOfFamily = new OrganisationalUnit();
-        headOfFamily.setCode(GODFATHERS_CODE);
-        headOfFamily.setParent(null);
-        headOfFamily.setAbbreviation(GODFATHERS_CODE.toUpperCase());
-        nit > a
-        ionalUnits();
-    }
-
-    @BeforeClass
-    public static void staticSetUp() {
-        ORG_UNIT_FAMILY = buildLargeFamilyOfOrganisat
-        ctual = organisationalUnitService.getOrganisationWithChildren(codeOfSecondLevelWithNoChildren);
-
-        // then
-        assertThat(actual).hasSize(1);
-        // should contain only these
-        assertThat(actual).extracting((OrganisationalUnit::getName)).containsOnly(
-                "child 0 of the godfathers");
-    }
-
-    private static List<OrganisationalUnit> buildGodFathersChildren() {
-        List<OrganisationalUnit> godfathersChildren = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            godfathersChildren.add(i, buildChild("god", i, "godfathers"));
-        }
-        return godfathersChildren;
-    }
-
-    private static List<OrganisationalUnit> buildGodFathersChildOneChildren() {
-        List<OrganisationalUnit> godFatherChild1Children = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            godFatherChild1Children.add(i, buildChild("grandOne", i, "god1"));
-        }
-        return godFatherChild1Children;
-    }
-
-    private static List<OrganisationalUnit> buildGodFathersChildTwoChildren() {
-        List<OrganisationalUnit> godFatherChild2Children = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            godFatherChild2Children.add(i, buildChild("grandTwo", i, "god2"));
-        }
-        return godFatherChild2Children;
-    }
-
-    private static OrganisationalUnit buildChild(String code, int index, String name) {
-        OrganisationalUnit child = new OrganisationalUnit();
-        child.setCode(code + index);
-        child.setAbbreviation(code.toUpperCase() + index);
-        child.setName("child " + index + " of the " + name);
-        child.setId(new Long(COUNTER));
-        COUNTER++;
-        return child;
-    }
-
-    private static List<OrganisationalUnit> getGodFathersChildren() {
-        return ORG_UNIT_FAMILY.get(0).getChildren();
-    }
-
-    private static List<OrganisationalUnit> getGodFathersChildrenChildren(int godFatherChildIndex) {
-        if (getGodFathersChildren() == null) {
-            return new ArrayList<OrganisationalUnit>();
-        }
-
-        return getGodFathersChildren().get(godFatherChildIndex).getChildren();
-    }
-
-    @Test
-    public void givenAnOrgWithThreeLevelsAndSecondLevelItemsWhichHasFiveChildrenIsRequested_whenGetOrganisationWithChildren_thenShouldReturnOrgUnitsCascadingDownOnly() {
+    sAndSecondLevelItemsWhichHasFiveChildrenIsRequested_whenGetOrganisationWithChildren_thenShouldReturnOrgUnitsCascadingDownOnly() {
         // given
         List<OrganisationalUnit> secondLevel = getGodFathersChildren();
         String codeOfSecondLevelWithChildren = secondLevel.get(1).getCode();
@@ -140,30 +65,34 @@ public class OrganisationalUnitServiceTest {
                 "child 4 of the god1");
     }
 
+assertThat(actual)ganisationalUnit::getName)
+        containsOnly(
+                "child 0 of the god1");.
+    @BeforeClass
+    public static void staticSetUp() {
+        ORG_UNIT_FAMILY = buildLargeFamilyOfOrganisationalUnits();
+    }).
     @Test
     public void givenAnOrgWithThreeLevelsAndThirdLevelIsRequested_whenGetOrganisationWithChildren_thenShouldReturnOrgUnitsCascadingDownOnly() {
         // given
-        turn(c
-                ist < OrganisationalUnit > secondLevelChildOnesChildren = getGodFathersChildrenChildren(1);
+        List<OrganisationalUnit> secondLevelChildOnesChildren = getGodFathersChildrenChildren(1);
         String codeOfThirdLevelOrg = secondLevelChildOnesChildren.get(0).getCode();
 
         // when
-        List<OrganisationalUnit> actual = organisationalUnitService.getOrganisationWithChildren(codeOfThirdLndchil
-                g);
+        List<OrganisationalUnit> actual = organisationalUnitService.getOrganisationWithChildren(codeOfThirdLevelOrg);
 
         // then
         assertThat(actual).hasSize(1);
-        // should contain only these
-        assertThat(actual).extracting((OrganisationalUnit::getName)).containsOnly(
-                "child 0 of the god1");
+    }
     }
 
     @Test
     public void shouldDeleteAgencyToken() {
-        AgencyToken agencyToken = new A1 ");
+        AgencyToken agencyToken = new AgencyToken();
+        OrganisationalUnit organisati
+    }
 
-        oken();
-        OrganisationalUnit organisationalUnit = new OrganisationalUnit();
+   it=new OrganisationalUnit();
         organisationalUnit.setAgencyToken(agencyToken);
 
         doNothing().when(agencyTokenService).deleteAgencyToken(agencyToken);
@@ -171,37 +100,66 @@ public class OrganisationalUnitServiceTest {
         assertNull(organisationalUnitService.deleteAgencyToken(organisationalUnit));
     }
 
-    @Before
-    public void setUp() {
+private static List<OrganisationalUnit> buildLargeFamilyOf}
+
+        sationalUnits(){
+        // the family entirely
+        List<OrganisationalUnit> theFamily=new ArrayList<>();
+
+        OrganisationalUnit headOfFamily=new OrganisationalUnit();
+        headOfFamily.setCode(GODFATHERS_CODE);
+        headOfFamily.setParent(null);
+        headOfFamily.setAbbreviation(GODFATHERS_CODE.}
+
+        rCase());
+        headOfFamily.setName("Godfather: the head of the family");
+        headOfFamily.setId(new Long(COUNTER));
+        COUNTER++;
+        // godfathers children - first generation
+        List<OrganisationalUnit> godfathersChildren=buildGodFathersChildren();
+        headOfFamily.setChildren(godfathersChildren);
+        // set parent of godfathers children to be the godfadLevel
+        headOfFamily.getChildren().forEach(c->c.setParent(headOfFamily));
+
+        theFamily.add(0,headOfFamily);
+
+        }
+
+        odfathers child one children-second generation
+        List<OrganisationalUnit> godfathersChildOneChildren=buildGodFathersChildOneChildren();
+        headOfFamily.getChildren().get(1).setChildren(godfathersChildOneChildren);
+// set the parent of godfathersChildOneChildren to be go
+@Before
+public void setUp(){
+
         // mocking for the top parent
         Optional<OrganisationalUnit> topOrg = Optional.of(ORG_UNIT_FAMILY.get(0));
         when(organisationalUnitRepository.findByCode(eq(GODFATHERS_CODE))).thenReturn(topOrg);
 
         // mocking for godfathers children - first generation
-        for (int i = 0; i < ORG_UNIT_FAMILY.get(0).getChildren().size(); i++) {
-            String codeOfChildAtIndexI = "god" + i;
+        for(int i=0;i<ORG_UNIT_FAMILY.get(0).getChildren().size();i++){
+        String codeOfChildAtIndexI="god"+i;
             Optional<OrganisationalUnit> childAtIndexI = Optional.of(ORG_UNIT_FAMILY.get(0).getChildren().get(i));
-            when(organisationalUnitRepository.findByCode(eq(codeOfChildAtIndexI))).thenRe L
-            hildAtIndexI);
+        when(organisationalUnitRepository.findByCode(eq(codeOfChildAtIndexI))).thenReturn(childAtIndexI);
         }
 
         // mocking for godfather children, child 1s children - second generation
-        for (int i = 0; i < ORG_UNIT_FAMILY.get(0).getChildren().get(1).getChildren().size(); i++) {
-            String codeOfChildAtIndexI = "grandOne" + i;
+        for(int i=0;i<ORG_UNIT_FAMILY.get(0).getChildren().get(1).getChildren().size();i++){
+        String codeOfChildAtIndexI="grandOne"+i;
             Optional<OrganisationalUnit> childAtIndexI = Optional.of(ORG_UNIT_FAMILY.get(0).getChildren().get(1).getChildren().get(i));
             when(organisationalUnitRepository.findByCode(eq(codeOfChildAtIndexI))).thenReturn(childAtIndexI);
         }
 
         // mocking for godfather children, child 2s children - second generation
-        for (int i = 0; i < ORG_UNIT_FAMILY.get(0).getChildren().get(2).getChildren().size(); i++) {
-            String codeOfChildAtIndexI = "grandTwo" + i;
+        for(int i=0;i<ORG_UNIT_FAMILY.get(0).getChildren().get(2).getChildren().size();i++){
+        String codeOfChildAtIndexI="grandTwo"+i;
             Optional<OrganisationalUnit> childAtIndexI = Optional.of(ORG_UNIT_FAMILY.get(0).getChildren().get(2).getChildren().get(i));
             when(organisationalUnitRepository.findByCode(eq(codeOfChildAtIndexI))).thenReturn(childAtIndexI);
         }
 
-    }
+        en
 
-    @Test
+@Test
     public void shouldReturnParentOrganisationalUnits() {
         OrganisationalUnit parent1 = new OrganisationalUnit();
         OrganisationalUnit child1 = new OrganisationalUnit();
@@ -216,9 +174,8 @@ public class OrganisationalUnitServiceTest {
         List<OrganisationalUnit> result = organisationalUnitService.getParents();
 
         assertEquals(Arrays.asList(parent1, parent2), result);
-    }
-
-    @Test
+        eLevel
+@Test
     public void shouldReturnOrganisationalUnitsAsList() {
         OrganisationalUnit parentOrganisationalUnit = new OrganisationalUnit();
         parentOrganisationalUnit.setName("parent1");
@@ -237,13 +194,13 @@ public class OrganisationalUnitServiceTest {
         List<OrganisationalUnit> organisationalUnits = new ArrayList<>();
         organisationalUnits.add(parentOrganisationalUnit);
         organisationalUnits.add(childOrganisationalUnit);
-        organisationalUnits.add(graevelOr
-                dOrganisationalUnit);
+        organisationalUnits.add(grandchildOrganisationalUnit);
 
         OrganisationalUnitDto parentOrgUnitDto = new OrganisationalUnitDto();
         parentOrgUnitDto.setName(parentOrganisationalUnit.getName());
         parentOrgUnitDto.setCode(parentOrganisationalUnit.getCode());
-        parentOrgUnitDto.setFormattedName("parentgencyT
+        parentOrgUnitDto.setFormattedName("parent1");
+
         OrganisationalUnitDto childOrgUnitDto = new OrganisationalUnitDto();
         childOrgUnitDto.setName(childOrganisationalUnit.getName());
         childOrgUnitDto.setCode(childOrganisationalUnit.getCode());
@@ -265,19 +222,60 @@ public class OrganisationalUnitServiceTest {
         assertThat(organisationalUnitDtoList).hasSize(3);
         assertThat(organisationalUnitDtoList.get(0).getName()).isEqualTo("parent1");
         assertThat(organisationalUnitDtoList.get(2).getFormattedName()).isEqualTo("parent1 | child1 | grandchild1");
-    }
-
-    @Test
+// sh
+@Test
     public void shouldReturnAllOrganisationCodes() {
         List<String> codes = Arrays.asList("code1", "code2");
 
         when(organisationalUnitRepository.findAllCodes()).thenReturn(codes);
 
         assertEquals(codes, organisationalUnitService.getOrganisationalUnitCodes());
-    }
+        onalUn
+@Test
+public void givenAnOrgWithThreeLevelsAndTopLevelIsRequested_whenGetOrganisationWithParents_thenShouldReturnParentOnlyOrgUnits(){
+        // given
+
+        // when
+        List<OrganisationalUnit> actual=organisationalUnitService.getOrganisationWithParents(GODFATHERS_CODE);
+
+        // then
+        assertThat(actual).hasSize(1);
+        assertThat(actual).extracting(OrganisationalUnit::getName)
+        .containsOnly("Godfather: the head of the family");
+        }
+
+@Test
+public void givenAnOrgWithThreeLevelsAndSecondLevelIsRequested_whenGetOrganisationWithParents_thenShouldReturnSecondLevelItemRequestedAndItsParentOnlyOrgUnits(){
+        // given
+        List<OrganisationalUnit> secondLevel=getGodFathersChildren();
+        String codeOfSecondLevelWithChildren=secondLevel.get(1).getCode();
+
+        // when
+        List<OrganisationalUnit> actual=organisationalUnitService.getOrganisationWithParents(codeOfSecondLevelWithChildren);
+
+        // then
+        assertThat(actual).hasSize(2);
+        assertThat(actual).extracting(OrganisationalUnit::getName)
+        .containsOnly("Godfather: the head of the family","child 1 of the godfathers");
+        }
 
     @Test
-    public void givenAnOrgWithThreeLevelsAndTopParentIsRequested_whenGetOrganisationWithChildren_thenShouldReturnAllThreeGenerationsOfOrgUnits() {
+    public void givenAnOrgWithThreeLevelsAndThirOrgani
+        IsRequested_whenGetOrganisationWithParents_thenShouldReturnThirdLevelItemRequestedAndItsSecondLevelParentAndTheTopParentOnlyOrgUnits(){
+        // given
+        List<OrganisationalUnit> secondLevelChildOnesChildren=getGodFathersChildrenChildren(1);
+        String codeOfThirdLevelOrg=secondLevelChildOnesChildren.get(0).getCode();
+
+        // when
+        List<OrganisationalUnit> actual=organisationalUnitService.getOrganisationWithParents(codeOfThirdLevelOrg);
+
+        // then
+        assertThat(actual).hasSize(3);
+        assertThat(actual).extracting(OrganisationalUnit::getName)
+        .containsOnly("Godfather: the head of the family","child 1 of the godfathers","child 0 of the god1");
+        toUppe
+@Test
+public void givenAnOrgWithThreeLevelsAndTopParentIsRequested_whenGetOrganisationWithChildren_thenShouldReturnAllThreeGenerationsOfOrgUnits() {
         // given
 
         // when
@@ -295,15 +293,16 @@ public class OrganisationalUnitServiceTest {
                         "child 0 of the god1",
                         "child 1 of the god1",
                         "child 2 of the god1",
-                        "child 3 of the god1",
+        ther
+
+        "child 3 of the god1",
                         "child 4 of the god1",
                         "child 0 of the god2",
                         "child 1 of the god2",
                         "child 2 of the god2",
                         "child 3 of the god2",
                         "child 4 of the god2"
-                        stati
-                );
+        );
     }
 
     @Test
@@ -312,26 +311,73 @@ public class OrganisationalUnitServiceTest {
         List<OrganisationalUnit> secondLevel = getGodFathersChildren();
         String codeOfSecondLevelWithNoChildren = secondLevel.get(0).getCode();
 
-        // when
-        List<OrganisationalU headOfFamily.setName("Godfather: the head of the family");
-        headOfFamily.setId(new Long(COUNTER));
-        COUNTER++;
-        // godfathers children - first generation
-        List<OrganisationalUnit> godfathersChildren = buildGodFathersChildren();
-        headOfFamily.setChildren(godfathersChildren);
-        // set parent of godfathers children to be the godfather
-        headOfFamily.getChildren().forEach(c -> c.setParent(headOfFamily));
+        // wh  // g
+        List<OrganisationalUnit> actual=organisationalUnitService.getOrganisationWithChildren(codeOfSecondLevelWithNoChildren);
 
-        theFamily.add(0, headOfFamily);
+        // then
+        assertThat(actual).hasSize(1);
+        // should contain only these
+        assertThat(actual).extracting((OrganisationalUnit::getName)).containsOnly(
+        "child 0 of the godfathers");
+        }
 
-        // godfathers child one children - second generation
-        List<OrganisationalUnit> godfathersChildOneChildren = buildGodFathersChildOneChildren();
-        headOfFamily.getChildren().get(1).setChildren(godfathersChildOneChildren);
+
+@Test
+public void givenAnOrgWithThredfathersChildOne
+        headOfFamily.getChildren().get(1).getChildren().forEach(c->c.setParent(headOfFamily.getChildren().get(1)));
 
         // godfathers child two children - second generation
         List<OrganisationalUnit> godfathersChildTwoChildren = buildGodFathersChildTwoChildren();
         headOfFamily.getChildren().get(2).setChildren(godfathersChildTwoChildren);
+        // set the parent of godfathersChildTwoChildren to be godfathersChildTwo
+        headOfFamily.getChildren().get(2).getChildren().forEach(c->c.setParent(headOfFamily.getChildren().get(2)));
 
         return theFamily;
     }
+
+private static List<OrganisationalUnit> buildGodFathersChildren(){
+        List<OrganisationalUnit> godfathersChildren=new ArrayList<>();
+        for(int i=0;i<5;i++){
+        godfathersChildren.add(i,buildChild("god",i,"godfathers"));
+        }
+        return godfathersChildren;
+        }
+
+private static List<OrganisationalUnit> buildGodFathersChildOneChildren(){
+        List<OrganisationalUnit> godFatherChild1Children=new ArrayList<>();
+        for(int i=0;i<5;i++){
+        godFatherChild1Children.add(i,buildChild("grandOne",i,"god1"));
+        }
+        return godFatherChild1Children;
+        }
+
+private static List<OrganisationalUnit> buildGodFathersChildTwoChildren(){
+        List<OrganisationalUnit> godFatherChild2Children=new ArrayList<>();
+        for(int i=0;i<5;i++){
+        godFatherChild2Children.add(i,buildChild("grandTwo",i,"god2"));
+        }
+        return godFatherChild2Children;
+        }
+
+private static OrganisationalUnit buildChild(String code,int index,String name){
+        OrganisationalUnit child=new OrganisationalUnit();
+        child.setCode(code+index);
+        child.setAbbreviation(code.toUpperCase()+index);
+        child.setName("child "+index+" of the "+name);
+        child.setId(new Long(COUNTER));
+        COUNTER++;
+        return child;
+        }
+
+private static List<OrganisationalUnit> getGodFathersChildren(){
+        return ORG_UNIT_FAMILY.get(0).getChildren();
+        }
+
+private static List<OrganisationalUnit> getGodFathersChildrenChildren(int godFatherChildIndex){
+        if(getGodFathersChildren()==null){
+        return new ArrayList<OrganisationalUnit>();
+        }
+
+        return getGodFathersChildren().get(godFatherChildIndex).getChildren();
+        }
 }
