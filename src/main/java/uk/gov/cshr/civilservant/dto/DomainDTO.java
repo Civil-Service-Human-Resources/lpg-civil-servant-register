428b432214694bd8e9d4af82028dcef2b11dddc9
bridgeth <b.hodgson@kainos.com>
2020-01-31 17:42:47 +0000

    LC-329 - Upated API and unit tests to have a DTO.

---
+++ src/main/java/uk/gov/cshr/civilservant/dto/DomainDTO.java
@@ -0,0 +1,11 @@
+package uk.gov.cshr.civilservant.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DomainDTO {
+
+    private String isWhiteListed;
+}
