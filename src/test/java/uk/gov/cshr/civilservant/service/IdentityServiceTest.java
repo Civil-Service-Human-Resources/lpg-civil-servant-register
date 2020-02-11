d8c4eb651733179b99c430f65b1b1ed5bc2e754d
bridgeth <b.hodgson@kainos.com>
2020-02-11 17:17:27 +0000

    LC-329 - Updated API and unit tests to return a string

--- src/test/java/uk/gov/cshr/civilservant/service/IdentityServiceTest.java
+++ src/test/java/uk/gov/cshr/civilservant/service/IdentityServiceTest.java
@@ -3,29 +3,29 @@ package uk.gov.cshr.civilservant.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.web.util.UriComponentsBuilder;
-import uk.gov.cshr.civilservant.dto.DomainDTO;
 import uk.gov.cshr.civilservant.service.identity.IdentityFromService;
 import uk.gov.cshr.civilservant.service.identity.IdentityService;
-
-import java.net.URI;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class IdentityServiceTest {
 
     private static final String FIND_BY_EMAIL_URL = "http://localhost/identity";
 
-    private static final String IS_WHITELISTED_URL = "http://localhost/domain/isWhitelisted";
+    private static final String IS_WHITELISTED_URL = "http://localhost:8080/domain/isWhitelisted";
 
     private IdentityService identityService;
+
+    private String domain = "domain.com";
+
+    private String EXPECTED_IS_WHITELISTED_URL = "http://localhost:8080/domain/isWhitelisted/domain.com/";
 
     @Mock
     private OAuth2RestOperations restOperations;
@@ -71,47 +71,40 @@ public class IdentityServiceTest {
     @Test
     public void shouldReturnTrueIfWhitelitsed() {
         // given
-        String domain = "myDomain";
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(IS_WHITELISTED_URL)
-                .queryParam("domain", domain);
-        DomainDTO dto = new DomainDTO();
-        dto.setIsWhiteListed("true");
-        ResponseEntity responseEntity = new ResponseEntity<DomainDTO>(dto, HttpStatus.OK);
-        when(restOperations.getForEntity(any(URI.class), any())).thenReturn(responseEntity);
+        when(restOperations.getForObject(anyString(), any())).thenReturn("true");
 
         // when
         boolean actual = identityService.isDomainWhiteListed(domain);
 
         // then
         assertTrue(actual);
+        verify(restOperations, times(1)).getForObject(eq(EXPECTED_IS_WHITELISTED_URL), any());
     }
 
     @Test
     public void shouldReturnFalseIfNotWhitelitsed() {
         // given
-        String domain = "myDomain";
-        DomainDTO dto = new DomainDTO();
-        dto.setIsWhiteListed("false");
-        ResponseEntity responseEntity = new ResponseEntity<DomainDTO>(dto, HttpStatus.OK);
-        when(restOperations.getForEntity(any(URI.class), any())).thenReturn(responseEntity);
+        when(restOperations.getForObject(anyString(), any())).thenReturn("false");
 
         // when
         boolean actual = identityService.isDomainWhiteListed(domain);
 
         // then
         assertFalse(actual);
+        verify(restOperations, times(1)).getForObject(eq(EXPECTED_IS_WHITELISTED_URL), any());
     }
 
     @Test
     public void shouldReturnFalseIfExceptionIsThrown() {
         // given
-        String domain = "myDomain";
-        when(restOperations.getForEntity(any(URI.class), any())).thenThrow(new RuntimeException());
+        when(restOperations.getForObject(anyString(), any())).thenThrow(new RuntimeException());
 
         // when
         boolean actual = identityService.isDomainWhiteListed(domain);
 
         // then
         assertFalse(actual);
+        verify(restOperations, times(1)).getForObject(eq(EXPECTED_IS_WHITELISTED_URL), any());
     }
+
 }
