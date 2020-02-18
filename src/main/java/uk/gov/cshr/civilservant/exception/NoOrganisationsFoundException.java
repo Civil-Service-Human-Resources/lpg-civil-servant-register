4a1c51f610f71c8b19ffb2e1aaac5ab777e6614a
bridgeth <b.hodgson@kainos.com>
2020-02-18 16:41:13 +0000

    LC-14 - Changes since git rebase.

--- src/main/java/uk/gov/cshr/civilservant/exception/NoOrganisationsFoundException.java
+++ src/main/java/uk/gov/cshr/civilservant/exception/NoOrganisationsFoundException.java
@@ -2,6 +2,6 @@ package uk.gov.cshr.civilservant.exception;
 
 public class NoOrganisationsFoundException extends RuntimeException {
     public NoOrganisationsFoundException(String domain) {
-        super(String.format("Not organisations found for domain: %s", domain));
+        super(String.format("No organisations found for domain: %s", domain));
     }
 }
