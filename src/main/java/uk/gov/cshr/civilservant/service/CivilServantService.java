42123ff62bfa7efc37dc25fd4315a6c5e73127ec
bridgeth <b.hodgson@kainos.com>
2020-06-01 11:32:05 +0100

    LC-329 - Added unit tests for Civil Servant Service.

--- src/main/java/uk/gov/cshr/civilservant/service/CivilServantService.java
+++ src/main/java/uk/gov/cshr/civilservant/service/CivilServantService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.cshr.civilservant.domain.CivilServant;
 import uk.gov.cshr.civilservant.exception.CSRSApplicationException;
+import uk.gov.cshr.civilservant.exception.CivilServantNotFoundException;
 import uk.gov.cshr.civilservant.repository.CivilServantRepository;
 
 @Slf4j
@@ -17,13 +18,17 @@ public class CivilServantService {
     }
 
     public String getCivilServantUid() throws CSRSApplicationException {
-       CivilServant cs = civilServantRepository.findByPrincipal()
-               .orElseThrow(() -> new CSRSApplicationException("No Civil Servant found", new RuntimeException()));
-       if(cs.getIdentity() != null && cs.getIdentity().getId() != null) {
-           return cs.getIdentity().getUid();
-       } else {
-           throw new CSRSApplicationException("No Civil Servant found", new RuntimeException());
-       }
+        try {
+            CivilServant cs = civilServantRepository.findByPrincipal()
+                    .orElseThrow(() -> new CivilServantNotFoundException());
+            if(cs.getIdentity() != null && cs.getIdentity().getUid() != null) {
+                return cs.getIdentity().getUid();
+            } else {
+                throw new CivilServantNotFoundException();
+            }
+        } catch (Exception e) {
+            throw new CSRSApplicationException("No Civil Servant found", e);
+        }
    }
 
 }
