300d1a584863248d3d38edea9ca023b5996a4ce2
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-07-14 08:51:53 +0200

    LC-440: Removed civil servant fetch endpoints since they are no longer used by reporting service

--- src/test/java/uk/gov/cshr/civilservant/mapping/RoleMappingHandlerMappingTest.java
+++ src/test/java/uk/gov/cshr/civilservant/mapping/RoleMappingHandlerMappingTest.java
@@ -1,52 +1,24 @@
 package uk.gov.cshr.civilservant.mapping;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.core.annotation.AnnotationUtils;
-import uk.gov.cshr.civilservant.controller.ReportController;
-
-import java.lang.reflect.Method;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.core.annotation.AnnotationUtils;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(AnnotationUtils.class)
 public class RoleMappingHandlerMappingTest {
 
     private RoleMappingHandlerMapping handlerMapping = new RoleMappingHandlerMapping();
-
-    @Test
-    public void shouldReturnRoleRequestConditionForClassAnnotation() {
-        String[] roles = {"test-role"};
-
-        RoleMapping annotation = mock(RoleMapping.class);
-        when(annotation.value()).thenReturn(roles);
-
-        Class type = ReportController.class;
-
-        mockStatic(AnnotationUtils.class);
-
-        when(AnnotationUtils.findAnnotation(type, RoleMapping.class)).thenReturn(annotation);
-
-        assertEquals(new RoleRequestCondition(roles), handlerMapping.getCustomTypeCondition(type));
-    }
-
-    @Test
-    public void shouldReturnNullIfAnnotationNotFoundOnClass() {
-        Class type = ReportController.class;
-
-        mockStatic(AnnotationUtils.class);
-
-        when(AnnotationUtils.findAnnotation(type, RoleMapping.class)).thenReturn(null);
-
-        assertNull(handlerMapping.getCustomTypeCondition(type));
-    }
 
     @Test
     public void shouldReturnRoleRequestConditionForMethodAnnotation() {
