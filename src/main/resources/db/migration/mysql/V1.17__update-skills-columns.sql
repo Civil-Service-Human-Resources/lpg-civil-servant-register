52a2a35e502215aab0af33ed1ec4dd9b95ff7c6a
kameshganesan <58697386+kameshganesan@users.noreply.github.com>
2020-08-20 09:35:46 +0100

    Update columns to fit question data and validation of fields (#132)

---
+++ src/main/resources/db/migration/mysql/V1.17__update-skills-columns.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `question` MODIFY `value` VARCHAR(500);
+
+ALTER TABLE `question` MODIFY `theme` VARCHAR(200);
+
+ALTER TABLE `submitted_answer` MODIFY `question` VARCHAR(MAX);
+
+SET FOREIGN_KEY_CHECKS = 1;
