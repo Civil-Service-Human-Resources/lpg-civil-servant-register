22df19e8f4cc256d094e255a280922f78dc578f8
kameshganesan <58697386+kameshganesan@users.noreply.github.com>
2020-08-20 13:50:27 +0100

    Unsetting MAX keyword and 15000 char limit for question (#133)

--- src/main/resources/db/migration/h2/V1.17__update-skills-columns.sql
+++ src/main/resources/db/migration/h2/V1.17__update-skills-columns.sql
@@ -4,6 +4,6 @@ ALTER TABLE `question` MODIFY `value` VARCHAR(500);
 
 ALTER TABLE `question` MODIFY `theme` VARCHAR(200);
 
-ALTER TABLE `submitted_answer` MODIFY `question` VARCHAR(MAX);
+ALTER TABLE `submitted_answer` MODIFY `question` VARCHAR(15000);
 
 SET FOREIGN_KEY_CHECKS = 1;
